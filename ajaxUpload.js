ae0a06386861d72317d7f54a5f7e0615a79e4e1a
craigcosmo <craigcosmo@gmail.com>
2015-12-16 11:58:25 +0700

    update

--- ajaxUpload.js
+++ ajaxUpload.js
@@ -7,17 +7,17 @@
 							'<div class="progress-bar">'+
 							'</div>'+
 						'</div>'+
-						'<a class="cancel" title="cancel"></a>'+
+						'<a class="cancel-upload" title="cancel"></a>'+
 					'</li>';
-			
+
+
 		var defaults = {
-			fileList : '#file-list',
-			listItem: html,
+			list : '',
+			item: html,
 			url : '',
-			progressor: '',
 			dropZone:'',
+			progressor: '',
 			progressBar:'.progress-bar',
-			cancelButton:'.cancel-upload',
 			onSubmit:function(){},
 			onProgress:function(){},
 			onComplete:function(){},
@@ -37,148 +37,199 @@
 		return this.each(function(){
 			
 			var input = $(this);
+			
 
 			function uploadFile(file){
-				$(o.fileList).append(o.listItem);
-				var li = $(o.fileList).children().last(),
-					cancel = li.find(o.cancelButton),
-					bar = li.find(o.progressBar),
-					progressor = li.find(o.progressor),
-					index = li.index(),
-					size = parseInt(file.size/1024,10),
-				 	name = file.name,
-					total,
-					loaded,
-					progress,
-					response;
-					
-				o.onSubmit.call(this, name, size, index);
-						
+				var item = $(o.item);
+				$(o.list).append(item);
+
+				var	bar = item.find(o.progressBar);
+				var	progressor = item.find(o.progressor).text('0%');
+				var	size = parseInt(file.size/1024,10);
+				var name = file.name;
+				var	object = item;
+
+
+
+				var	total;
+				var	loaded = 0;
+				var	progress = '0%';
+				var	progessFlow = '0%';
+				// this path show the temporary file on browser
+				// which can be access via image src
+				var	path = URL.createObjectURL(file);
+				var	index = item.index();
+				var	response;
 				var xhr = new XMLHttpRequest();
-				progressor.text('0%');
-				// Update progress bar
-				xhr.upload.addEventListener("progress", function (e) {
-					
-					if(bar.length){
-						bar[0].style.width = (e.loaded / e.total) * 100 + "%";
-					}
-					progress = Math.round((e.loaded / e.total) * 100) + "%";
-					loaded = parseInt(e.loaded/1024,10);
-					total = e.total *1024;
-					if(progressor.length){
-						progressor.text(progress);
-					}
-					o.onProgress.call(this, name, size, index, loaded, progress);
-				}, false);
+
+				object.name = name;
+				object.size = size;
+				object.path = path;
+				object.index = index;
+				object.progress = progress;
+				object.progessFlow = progessFlow;
+				object.loaded = loaded;
+
+				o.onSubmit.call(this, object);
+						
 				
-				// File uploaded
-				xhr.addEventListener("load", function () {
-					if(bar.length){
-						//update progress bar to 100% in firefox
-						//update progress number to display in the view
-						progress = bar[0].style.width = 100 + "%";
-					}
-					if(progressor.length){
-						// progressor.text(progress).hide();
-						progressor.text(progress);
-					}
-					if(cancel.length){
-						cancel.hide();
-					}
-					// loaded should be equal total by now
-					loaded = total;
 
-					// response = JSON.parse(xhr.responseText);   // commented out for testing
-					// o.onComplete.call(this, name, size, index, response); // commented out for testing
+				// xhr.upload.addEventListener("progress", function (e) { }, false);
+				xhr.upload.onprogress =function(e){
+					object.progessFlow = progessFlow = (e.loaded / e.total) * 100 + "%";
+					object.progress = progress = Math.round((e.loaded / e.total) * 100) + "%";
+					object.loaded = loaded = parseInt(e.loaded/1024,10);
+					// total = e.total *1024;
 
+					if(bar.length) bar[0].style.width = progessFlow;
+					if(progressor.length) progressor.text(progress);
 
-					// check if response is json object
-					var isJSON = true;
-					try {
-					    $.parseJSON(xhr.responseText);
-					} catch (e) {
-					    isJSON = false;
-					}
-
-					if(isJSON == true){
-						response = $.parseJSON(xhr.responseText);
-						o.onComplete.call(this, name, size, index, response);
-					}else{
-						response = xhr.responseText;
-						o.onComplete.call(this, name, size, index, response);
-					}
-					
-					
-				}, false);
-
-				if(cancel.length){
-					cancel[0].addEventListener("click", function(){
-						xhr.abort();
-						o.onCancel.call(this, name, size, index, loaded, progress);
-					}, false);
+					o.onProgress.call(this, object);
 				}
+					
+		
+				// xhr.addEventListener("load", function () {}, false);
+				xhr.onload = function(){
+					//update progress bar to 100% in firefox
+					//update progress number to display in the view
+					if(bar.length) progress = bar[0].style.width = 100 + "%";
+					if(progressor.length) progressor.text(progress);
+					// loaded should be equal total by now
+					// loaded = total;
+
+					// check if response is json object or text
+					response = sortResponse(xhr.responseText);
+					o.onComplete.call(this, object, response);
+				}
+
+				// xhr.responseType
+				// xhr.response contain the value fetched from server
+				 // var blob = new Blob([this.response], {type: 'image/png'});
+
+				 // var blob = this.response;
+
+			  //   var img = document.createElement('img');
+			  //   img.onload = function(e) {
+			  //     window.URL.revokeObjectURL(img.src); // Clean up after yourself.
+			  //   };
+			  //   img.src = window.URL.createObjectURL(blob);
+			  //   document.body.appendChild(img);
 
 				xhr.open("post", o.url, true);
 				
 				// Set appropriate headers
 				// xhr.setRequestHeader("Cache-Control", "no-cache");
-    // 			xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+			    // xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
 				xhr.setRequestHeader("Content-Type", "multipart/form-data"); 
-				xhr.setRequestHeader("X-File-Name", file.name);
+
+				// xhr.setRequestHeader('Content-Type', mimeType);
+				// xhr.setRequestHeader('Content-Disposition', 'attachment; filename="' + file.name + '"');
+				// xhr.setRequestHeader("X-File-Name", file.name);
+				xhr.setRequestHeader("X-File-Name", unescape(encodeURIComponent(file.name)));
+				// fixed vietnamese character file name bug
 				xhr.setRequestHeader("X-File-Size", file.size);
 				xhr.setRequestHeader("X-File-Type", file.type);
 	
+				// var fd = new FormData();
+				// fd.append("upload_file", file);
+				// xhr.send(fd);
+
 				// Send the file
 				xhr.send(file);
+
+				return xhr;
 			}
+
 			
+
 			function traverseFiles (files) {
 				for (var i=0, l=files.length; i<l; i++) {
-					if(o.allowedType.length && o.allowedType.match(files[i].type.toLowerCase().replace('image/', ''))==null){
-						o.onTypeError.call(this, files[i].name);
+
+					var type= getTrueFileType(files[i]);
+					// console.log(type);
+
+					if(o.allowedType.length && !o.allowedType.match(type)){
+						o.onTypeError.call(this, {name: files[i].name} );
 					}
 					else if(o.maxSize >0 && parseInt(files[i].size/1024) > o.maxSize){
-						o.onSizeError.call(this, files[i].name);
+						o.onSizeError.call(this, {name: files[i].name});
 					}
-					else if(o.maxLength>0 && $(o.fileList).children().length >= o.maxLength){
-						o.onLengthError.call(this, $(o.fileList).children().length);
+					else if(o.maxLength>0 && $(o.list).children().length >= o.maxLength){
+						o.onLengthError.call(this, $(o.list).children().length);
 					}
 					else if (typeof FileReader !== "undefined"){
+
 						getImgSize(files[i], function(file, width, height){
+							// console.log(file.type);
 							if(o.minWidth>0 && width<o.minWidth){
-								o.onDimensionError.call(this, file.name, width, height);
+								o.onDimensionError.call(this, {name :file.name, width: width, height: height});
 							}
 							else if(o.minHeight>0 && height<o.minHeight){
-								o.onDimensionError.call(this, file.name, width, height);
+								o.onDimensionError.call(this, {name :file.name, width: width, height: height});
 							}
-							else if($(o.fileList).children().length >= o.maxLength){//needed double check items length
-								o.onLengthError.call(this, $(o.fileList).children().length);	
+							else if($(o.list).children().length >= o.maxLength){//needed double check items length
+								o.onLengthError.call(this, $(o.list).children().length);	
 							}
 							else {
 								uploadFile(file);
+								// console.log(i);
+								// console.log(files[i]);
+								// uploadFile(files[i]);
+
 							}
 						});
 					}
 				}
 			}
 			
-			function getImgSize(file, whenReady){
+			function getImgSize(file, callback){
 				var reader = new FileReader();  
 				reader.onload = function(evt) {
 					var image = new Image();
 					image.onload = function(evt) {
 						var width = this.width;
 						var height = this.height;
-						if (whenReady) whenReady(file, width, height);
+						if (callback) callback(file, width, height);
 					};
 					image.src = evt.target.result; 
 				};
 				reader.readAsDataURL(file);
 			}
 
-			input[0].addEventListener("change", function () {
-				traverseFiles(this.files);
+			function getTrueFileType(file){
+				// console.log(file.type);
+				if(file.type.length>0) {
+					return file.type.split('/')[1].toLowerCase().trim();
+				}else{ return '';}
+				// some file might have empty string, emptry doesn't work with split, throw error
+			}
+
+			// check if response is json object
+
+			function sortResponse(response){
+				var isJSON = true;
+
+				try {$.parseJSON(response);} catch (e) {isJSON = false;}
+
+				// response = JSON.parse(xhr.responseText);
+				if(isJSON == true){ sorted = $.parseJSON(response);}
+				else{sorted = response;}
+
+				return sorted;
+			}
+
+
+			// cancel
+			// call cancel by trigger cancel event on input
+			input[0].addEventListener('cancel', function(){
+				uploadFile.abort();
+				o.onCancel.call(this);
 			}, false);
+
+			input[0].addEventListener("change", function () {
+					traverseFiles(this.files);
+			}, false);
+			
 			
 			// drag and drop file
 			// detect dropzone, if there is no dropzone, create a random element for the event to bind with
