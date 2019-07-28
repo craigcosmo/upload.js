d2848fc80d1827c3892e8c06feece53b46b313ca
Craig Nguyen <craigcosmo@gmail.com>
2019-07-28 18:55:15 +0700

    update

--- upload.js
+++ upload.js
@@ -1,8 +1,5 @@
-
-class Upload{
-	
-	constructor(options){
-
+class Upload {
+	constructor(options) {
 		this.template = `<li>
 							<div class="progress-container">
 								<div class="progressor"></div>
@@ -14,33 +11,33 @@ class Upload{
 							</div>
 						</li>`
 
-
 		this.defaults = {
-			list : '',
+			list: '',
 			item: this.template,
-			url : '',
-			dropZone:'',
+			uploadingStaff: false,
+			url: '',
+			dropZone: '',
 			progressor: '',
-			progressBar:'.progress-bar',
-			onSubmit:function(){},
-			onProgress:function(){},
-			onComplete:function(){},
-			onSizeError:function(){},
-			onTypeError:function(){},
-			onLengthError:function(){},
-			onDimensionError:function(){},
-			onCancel:function(){},
-			onCancelAll:function(){},
-			onPause:function(){},
-			onQueueComplete:function(){},
-			allowedType:0,
-			maxLength:0,
-			minWidth:0,
-			minHeight:0,
-			minSize:0,
-			maxSize:0,
-			formData:true,
-			form: document.createElement('form')
+			progressBar: '.progress-bar',
+			onSubmit: function() {},
+			onProgress: function() {},
+			onComplete: function() {},
+			onSizeError: function() {},
+			onTypeError: function() {},
+			onLengthError: function() {},
+			onDimensionError: function() {},
+			onCancel: function() {},
+			onCancelAll: function() {},
+			onPause: function() {},
+			onQueueComplete: function() {},
+			allowedType: 0,
+			maxLength: 0,
+			minWidth: 0,
+			minHeight: 0,
+			minSize: 0,
+			maxSize: 0,
+			formData: true,
+			form: document.createElement('form'),
 		}
 
 		this.option = Object.assign(this.defaults, options)
@@ -48,8 +45,7 @@ class Upload{
 		this.ongoingCollection = []
 		this.completedCollection = []
 	}
-	uploadFile(file){
-
+	uploadFile(file) {
 		let list = this.list
 		let object = this.createDomFromString(this.option.item)
 		list.appendChild(object)
@@ -58,19 +54,19 @@ class Upload{
 		let progressor = object.querySelector(this.option.progressor)
 		progressor.innerHTML = '0%'
 
-		let	size = parseInt(file.size/1024,10)
+		let size = parseInt(file.size / 1024, 10)
 		let name = file.name
-		let	total
-		let	loaded = 0
-		let	progress = '0%'
-		let	progessFlow = '0%'
-		let	path = URL.createObjectURL(file)
-		let	index = Array.prototype.slice.call(list.children).indexOf(object)
-		let	response
+		let total
+		let loaded = 0
+		let progress = '0%'
+		let progessFlow = '0%'
+		let path = URL.createObjectURL(file)
+		let index = Array.prototype.slice.call(list.children).indexOf(object)
+		let response
 		let xhr = new XMLHttpRequest()
 		this.ongoingCollection.push(xhr)
 
-		console.log(index)
+		// console.log(index)
 
 		object.name = name
 		object.size = size
@@ -81,122 +77,142 @@ class Upload{
 		object.loaded = loaded
 
 		this.option.onSubmit.call(this, object)
-				
-		
+
 		xhr.upload.onprogress = (e) => {
 			object.progessFlow = progessFlow = (e.loaded / e.total) * 100 + '%'
-			object.progress = progress = Math.round((e.loaded / e.total) * 100) + '%'
-			object.loaded = loaded = parseInt(e.loaded/1024,10)
+			object.progress = progress =
+				Math.round((e.loaded / e.total) * 100) + '%'
+			object.loaded = loaded = parseInt(e.loaded / 1024, 10)
 			// total = e.total *1024;
 
 			// if(bar.length) bar.style.width = progessFlow;
-			if(document.contains(bar)) bar.style.width = progessFlow
-			if(document.contains(progressor)) progressor.innerHTML = progress
+			if (document.contains(bar)) bar.style.width = progessFlow
+			if (document.contains(progressor)) progressor.innerHTML = progress
 
 			this.option.onProgress.call(this, object)
-
 		}
 
 		xhr.onload = () => {
-			if(document.contains(bar)) progress = bar.style.width = 100 + '%'
-			if(document.contains(progressor)) progressor.innerHTML = progress
+			if (document.contains(bar)) progress = bar.style.width = 100 + '%'
+			if (document.contains(progressor)) progressor.innerHTML = progress
 
 			// check if response is json object or text
 			response = this.sortResponse(xhr.responseText)
 			this.option.onComplete.call(this, object, response)
 			this.completedCollection.push(object.index)
-			if (this.completedCollection.length === this.ongoingCollection.length) {
+			if (
+				this.completedCollection.length ===
+				this.ongoingCollection.length
+			) {
 				this.option.onQueueComplete.call(this)
 			}
 		}
 
-
 		xhr.open('post', this.option.url, true)
 
 		if (this.option.formData === false) {
-			xhr.setRequestHeader('Content-Type', 'multipart/form-data') 
-			// xhr.setRequestHeader('Content-Type', 'multipart/form-data') 
+			xhr.setRequestHeader('Content-Type', 'multipart/form-data')
+			// xhr.setRequestHeader('Content-Type', 'multipart/form-data')
 			// fix unicode character file name bug
-			xhr.setRequestHeader('X-File-Name', unescape(encodeURIComponent(file.name))) 
+			xhr.setRequestHeader(
+				'X-File-Name',
+				unescape(encodeURIComponent(file.name))
+			)
 			xhr.setRequestHeader('X-File-Size', file.size)
 			xhr.setRequestHeader('X-File-Type', file.type)
 			xhr.send(file)
-		}else{
-			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-			let FD  = new FormData(this.option.form)
+		} else {
+			// xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+			let FD = new FormData()
+			// let Form = this.option.form
+			// let input = Form.getElementById('upload')
+
+			let category = 'jackpot'
+			if (this.option.uploadingStaff) {
+				category = $('#sid').val()
+			}
+			FD.append('category', 'jackpot')
+			FD.append('file', file)
+
+			// console.log('form', FD)
 			xhr.send(FD)
 		}
-		
-
-
-		
 
 		return xhr
-		
 	}
-	cancel(object){
+	cancel(object) {
 		// need to check first
 		this.ongoingCollection[object.index].abort()
 		this.ongoingCollection.splice(object.index, 1)
 		this.option.onCancel.call(this, object)
-		
 	}
-	validate(file){
+	validate(file) {
 		let valid = true
 
-		let type= this.getFileType(file)
+		let type = this.getFileType(file)
 
 		var itemCount = this.list.children.length
 
-		if(this.option.allowedType.length && !this.option.allowedType.match(type)){
-			this.option.onTypeError.call(this, {name: file.name} )
+		if (
+			this.option.allowedType.length &&
+			!this.option.allowedType.match(type)
+		) {
+			this.option.onTypeError.call(this, { name: file.name })
 			valid = false
 		}
 
-		if(this.option.maxSize >0 && parseInt(file.size/1024) > this.option.maxSize){
-			this.option.onSizeError.call(this, {name: file.name})
+		if (
+			this.option.maxSize > 0 &&
+			parseInt(file.size / 1024) > this.option.maxSize
+		) {
+			this.option.onSizeError.call(this, { name: file.name })
 			valid = false
 		}
-		if(this.option.maxLength>0 && itemCount >= this.option.maxLength){
+		if (this.option.maxLength > 0 && itemCount >= this.option.maxLength) {
 			this.option.onLengthError.call(this, itemCount)
 			valid = false
 		}
-		
-		this.getImageSize(file, (file, width, height) => {
 
-			if(this.option.minWidth>0 && width<this.option.minWidth){
-				this.option.onDimensionError.call(this, {name :file.name, width: width, height: height})
+		this.getImageSize(file, (file, width, height) => {
+			if (this.option.minWidth > 0 && width < this.option.minWidth) {
+				this.option.onDimensionError.call(this, {
+					name: file.name,
+					width: width,
+					height: height,
+				})
 				valid = false
 			}
-			if(this.option.minHeight>0 && height<this.option.minHeight){
-				this.option.onDimensionError.call(this, {name :file.name, width: width, height: height})
+			if (this.option.minHeight > 0 && height < this.option.minHeight) {
+				this.option.onDimensionError.call(this, {
+					name: file.name,
+					width: width,
+					height: height,
+				})
 				valid = false
 			}
-			if(itemCount >= this.option.maxLength){//needed double check items length
-				this.option.onLengthError.call(this, itemCount)	
+			if (itemCount >= this.option.maxLength) {
+				//needed double check items length
+				this.option.onLengthError.call(this, itemCount)
 				valid = false
 			}
 		})
 
 		return valid
-
 	}
 	traverseFile(files) {
-		for (let i=0; i<files.length; i++) {
+		for (let i = 0; i < files.length; i++) {
 			this.validate(files[i]) && this.uploadFile(files[i])
 		}
 	}
-	isImage(img){
-
-	}
-	createDomFromString(string){
+	isImage(img) {}
+	createDomFromString(string) {
 		let div = document.createElement('div')
 		div.innerHTML = string
 		return div.firstChild
 	}
 
-	getImageSize(file, callback){
-		let reader = new FileReader()  
+	getImageSize(file, callback) {
+		let reader = new FileReader()
 		reader.onload = function(evt) {
 			let image = new Image()
 			image.onload = function(evt) {
@@ -204,24 +220,29 @@ class Upload{
 				let height = this.height
 				callback && callback(file, width, height)
 			}
-			image.src = evt.target.result 
+			image.src = evt.target.result
 		}
 		reader.readAsDataURL(file)
 	}
 
-	getFileType(file){
-		if(file.type.length>0) {
-			return file.type.split('/')[1].toLowerCase().trim()
-		}else{ return ''}
+	getFileType(file) {
+		if (file.type.length > 0) {
+			return file.type
+				.split('/')[1]
+				.toLowerCase()
+				.trim()
+		} else {
+			return ''
+		}
 		// some file might have empty string, emptry string doesn't work with split, so throw error
 	}
-	sortResponse(response){
+	sortResponse(response) {
 		let isJSON = true
-		try {JSON.parse(response)} catch (e) {isJSON = false}
+		try {
+			JSON.parse(response)
+		} catch (e) {
+			isJSON = false
+		}
 		return isJSON ? JSON.parse(response) : response
 	}
 }
-
-
-
-
